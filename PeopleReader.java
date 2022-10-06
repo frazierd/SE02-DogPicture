eb954e700eb8becdcff60368781dece5a2fbe71c
Frazier Dyson <frazier.dyson@gmail.com>
2022-10-06 11:03:02 -0500

    almost done, still need some logic on arranging by fear

--- PeopleReader.java
+++ PeopleReader.java
@@ -13,12 +13,48 @@ public class PeopleReader {
 
     public List<Person> getAllPeople() throws IOException {
         List<Person> people = new ArrayList<>();
+        Person person = new Person(firstName, lastName, middleName, age, fear);
 
         List<String> lines = Files.readAllLines(Path.of(filename));
         for (String line : lines) {
-            people.add(new Person(line));
-        }
+            String index[] = line.split(" ");
 
+            String firstName = index[0];
+            if(isInt(index[2])== true){
+                String lastName = index[1];
+                int age = Integer.parseInt(index[2]);
+                String fear = index[3];
+                if(index.length == 5){
+                    fear = index[3] + " " + index[4];
+                }
+                else{
+                    fear = index[3];
+                }
+            }
+            else{
+                String middleName = index[1];
+                String lastName = index[2];
+                int age = Integer.parseInt(index[3]);
+                String fear = index[4];
+                if(index.length == 6){
+                    fear = index[4] + " " + index[5];
+                }
+                else{
+                    fear = index[4];
+                }
+                people.add(new Person(firstName, middleName, lastName, age, fear));
+
+            }
+            Person person = new Person(firstName, lastName, firstName, 0, firstName);
+        }
         return people;
+    }
+    public boolean isInt(String str){
+        try {
+            Integer.parseInt(str);
+        }catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
--- Person.java
+++ Person.java
@@ -1,16 +1,36 @@
 public class Person {
     private final String firstName;
+    private final String lastName;
+    private final String middleName;
+    private final int age;
+    private final String fear;
 
-    public Person(String firstName) {
+    public Person(String firstName, String lastName, String middleName, int age, String fear) {
         this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.age = age;
+        this.fear = fear;
     }
 
     public String getFirstName() {
         return firstName;
     }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getMiddleName() {
+        return middleName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getFear() {
+        return fear;
+    }
 
     @Override
     public String toString() {
-        return String.format("Person [firstName=%s]", this.firstName);
+        return String.format( "%s %s %s %d %s", this.firstName, this.middleName, this.lastName, this.age, this.fear);
     }
 }
